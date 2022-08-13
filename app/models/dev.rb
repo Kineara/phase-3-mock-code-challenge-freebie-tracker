fb973e2661849957996b01e9c32b446010ea0c63
Lauren Hoock <laurenh8916@protonmail.com>
2022-08-13 03:05:24 -0600

    completed

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,13 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
 
+  def received_one?(item_name)
+    self.freebies.exists?(item_name: item_name)
+  end
+
+  def give_away(dev, freebie)
+    gift = self.freebies.find(freebie.id)
+    gift.update(dev_id: dev.id)
+  end
 end
